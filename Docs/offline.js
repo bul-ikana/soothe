ff0e8ac71d2f4d302740f3d6f5b98d72c7656a97
Hugo Aguirre <bul.ikana@gmail.com>
2017-05-21 23:30:36 -0500

    game preview

---
+++ Docs/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1495419223,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/tilemap.png",
+		"media/static.ogg",
+		"media/vapor.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
